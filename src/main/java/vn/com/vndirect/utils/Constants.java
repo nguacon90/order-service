1e956b66b77d297b18964e8d453c62b6c68d0044
Quang Minh <minhtq90@gmail.com>
2014-07-31 01:02:56 +0700

    add Constants file

---
+++ src/main/java/vn/com/vndirect/utils/Constants.java
@@ -0,0 +1,9 @@
+package vn.com.vndirect.utils;
+
+public class Constants {
+	public static final String ROOT_CONTEXT_PATH = "/rest";
+	public static final String SPRING_CONTEXT_LISTENER = "org.springframework.web.context.ContextLoaderListener";
+	public static final String PACKAGE_RESOURCES = "vn.com.vndirect.resources";
+	public static final String CONTEXT_LOCATION_PARAM = "contextConfigLocation";
+	
+}
